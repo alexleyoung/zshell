3e09ba4452efe76287dc24652f9f29dcf6a9ba6a
Alex Young <alexyoung0119@gmail.com>
2026-05-02 18:34:27 -0500

    get input

--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,12 @@ const std = @import("std");
 pub fn main(init: std.process.Init) !void {
     var stdout = std.Io.File.stdout().writer(init.io, &.{});
 
-    // Suppress unused local constant error. Feel free to remove the line below.
-    try stdout.interface.print("", .{});
+    var stdin_buffer: [4096]u8 = undefined;
+    var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
-    // TODO: Uncomment the code below to pass the first stage
-    try stdout.interface.print("$ ", .{});
+    while (true) {
+        try stdout.interface.print("$ ", .{});
+        const command = try stdin.interface.takeDelimiter('\n');
+        try stdout.interface.print("{s}: command not found\n", .{command.?});
+    }
 }
