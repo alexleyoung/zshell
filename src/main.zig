513a7778fb0478e8eb3072504a54801bb3d63117
Alex Young <alexyoung0119@gmail.com>
2026-05-02 19:11:47 -0500

    refactor

--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,43 @@
 const std = @import("std");
 
+const Command = enum {
+    echo,
+    exit,
+    type,
+    invalid,
+
+    pub fn fromString(str: []const u8) ?Command {
+        const command = std.meta.stringToEnum(Command, str);
+        return command;
+    }
+};
+
 pub fn main(init: std.process.Init) !void {
     var stdout = std.Io.File.stdout().writer(init.io, &.{});
-
     var stdin_buffer: [4096]u8 = undefined;
     var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
     while (true) {
         try stdout.interface.print("$ ", .{});
-        const command = try stdin.interface.takeDelimiter('\n');
 
-        var parts = std.mem.splitScalar(u8, command.?, ' ');
+        const input = try stdin.interface.takeDelimiter('\n') orelse "";
 
-        if (std.mem.eql(u8, "exit", parts.peek().?)) {
-            break;
-        } else if (std.mem.eql(u8, "echo", parts.peek().?)) {
-            try stdout.interface.print("{s}\n", .{command.?[5..]});
-        } else if (std.mem.eql(u8, "type", parts.peek().?)) {
-            _ = parts.next();
-            // check if built-in
-            if (std.mem.eql(u8, "exit", parts.peek().?) or
-                std.mem.eql(u8, "echo", parts.peek().?) or
-                std.mem.eql(u8, "type", parts.peek().?))
-            {
-                try stdout.interface.print("{s} is a shell builtin\n", .{parts.peek().?});
-            } else {
-                try stdout.interface.print("{s}: not found\n", .{parts.peek().?});
-            }
-        } else {
-            try stdout.interface.print("{s}: command not found\n", .{command.?});
+        var iterator = std.mem.splitAny(u8, input, " ");
+        const command = Command.fromString(iterator.next().?) orelse .invalid;
+        const args = iterator.rest();
+
+        switch (command) {
+            .echo => {
+                try stdout.interface.print("{s}\n", .{args});
+            },
+            .exit => {
+                break;
+            },
+            .type => if (Command.fromString(args) != null)
+                try stdout.interface.print("{s} is a shell builtin\n", .{args})
+            else
+                try stdout.interface.print("{s}: not found\n", .{args}),
+            .invalid => try stdout.interface.print("{s}: command not found\n", .{input}),
         }
     }
 }
