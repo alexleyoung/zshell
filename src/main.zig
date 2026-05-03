20e109ff7364fc270cfa7ccdb8b217121dca9ffe
Alex Young <alexyoung0119@gmail.com>
2026-05-02 20:27:12 -0500

    type check $PATH

--- src/main.zig
+++ src/main.zig
@@ -16,27 +16,47 @@ pub fn main(init: std.process.Init) !void {
     var stdout = std.Io.File.stdout().writer(init.io, &.{});
     var stdin_buffer: [4096]u8 = undefined;
     var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
+    const env = init.environ_map;
 
     while (true) {
         try stdout.interface.print("$ ", .{});
 
         const input = try stdin.interface.takeDelimiter('\n') orelse "";
 
-        var iterator = std.mem.splitAny(u8, input, " ");
+        var iterator = std.mem.splitScalar(u8, input, ' ');
         const command = Command.fromString(iterator.next().?) orelse .invalid;
         const args = iterator.rest();
 
         switch (command) {
-            .echo => {
-                try stdout.interface.print("{s}\n", .{args});
+            .echo => try stdout.interface.print("{s}\n", .{args}),
+            .exit => break,
+            .type => {
+                if (Command.fromString(args) != null) {
+                    try stdout.interface.print("{s} is a shell builtin\n", .{args});
+                } else {
+                    // check path
+                    const path = env.get("PATH").?;
+                    const path_sep = std.Io.Dir.path.delimiter;
+
+                    var paths = std.mem.splitScalar(u8, path, path_sep);
+
+                    var found = false;
+                    while (paths.next()) |p| {
+                        const dir = try std.Io.Dir.openDirAbsolute(init.io, p, .{
+                            .access_sub_paths = false,
+                        });
+                        defer dir.close(init.io);
+                        dir.access(init.io, args, .{ .execute = true }) catch continue;
+                        try stdout.interface.print("{s} is {s}/{s}\n", .{ args, p, args });
+                        found = true;
+                        break;
+                    }
+
+                    if (!found) {
+                        try stdout.interface.print("{s}: not found\n", .{args});
+                    }
+                }
             },
-            .exit => {
-                break;
-            },
-            .type => if (Command.fromString(args) != null)
-                try stdout.interface.print("{s} is a shell builtin\n", .{args})
-            else
-                try stdout.interface.print("{s}: not found\n", .{args}),
             .invalid => try stdout.interface.print("{s}: command not found\n", .{input}),
         }
     }
